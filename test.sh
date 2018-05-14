ec0097ebd016ec17354bf887ec5881b76a12a10e
Xing Yong <xingyong@gmail.com>
2018-05-14 05:32:51 -0700

    new line

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/python
+import os
+import json
 
 #test
-
 print("hello world")
 
 #insert new line withtou LFCR settings
@@ -11,3 +12,9 @@ print("hahahaha")
 
 #插入一点中文试试呢
 print("你好~")
+#结论，diff无效和中文无关啊...
+
+
+#now add LFCT setting
+#额...忘记了，我的git早已设置过git config --global core.autocrlf input
+#所以，上面的commit都已经被git做了自动换行符转换
