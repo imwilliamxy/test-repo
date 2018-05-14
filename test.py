6f986d53e5104deddd58fd17aa787c0deb86fdce
Xing Yong <xingyong@gmail.com>
2018-05-14 05:36:39 -0700

    add utf8 comment

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #!/bin/python
+# -*- coding: utf-8 -*-  
 import os
 import json
 
@@ -18,3 +19,5 @@ print("你好~")
 #now add LFCT setting
 #额...忘记了，我的git早已设置过git config --global core.autocrlf input
 #所以，上面的commit都已经被git做了自动换行符转换
+
+#最后，加上utf8注释试试
